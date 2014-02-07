70aab3c7de4fb484a6764610d006f965d3352c0a
bougoue <oscar@blixtra.se>
2014-02-07 16:05:01 +0100

    Added intro and basic structure

---
+++ js/intro.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+  
+	// Application initalization
+	$("#main, #footer").hide();
+
+	$( "#startButton" ).click(function() {
+	  
+		$( "#intro" ).fadeOut( "slow", function() {
+			$("#main, #footer").fadeIn( "slow", function() {
+				// Fade in ready!
+			});
+		});
+	});
+	
+});
+
