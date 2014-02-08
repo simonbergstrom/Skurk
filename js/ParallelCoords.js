cc68956551667eecf36b5255aa7f182c6a160cc4
johnhollen <johnhollen982@gmail.com>
2014-02-08 12:42:13 +0100

    Minor Changes

--- js/ParallelCoords.js
+++ js/ParallelCoords.js
@@ -3,4 +3,8 @@ function ParallelCoords()
 	var self = this; //Used for internal d3-functions.
 
 	var parallelDiv = $("#parallelCoords");
+
+	console.log(parallelDiv);
+
+	parallelDiv.innerHTML = "Tjena";
 }
