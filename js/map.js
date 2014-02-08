6ca7b72119bf322455e1f06ae61f177fbf4fc4a8
bougoue <oscar@blixtra.se>
2014-02-08 13:27:56 +0100

    Minor.

--- js/map.js
+++ js/map.js
@@ -1,5 +1,26 @@
 function Map(){
 
-	
+	// Initalization
+
+
+
+
+
+
+    d3.json("data/kommuner_swe.json", function(error, world) {
+        var countries = topojson.feature(world, world.objects.countries).features;
+
+        
+
+        console.log(countries);
+        
+    });
+
+
+
+
+
+
+
 
 }
