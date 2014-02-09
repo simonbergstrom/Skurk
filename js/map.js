7d8676426f1ec6a074f7e42ba47995e073784b9d
bougoue <oscar@blixtra.se>
2014-02-09 20:38:17 +0100

    Added map that probably works.

--- js/map.js
+++ js/map.js
@@ -2,12 +2,12 @@ function Map(){
 
 	// Initalization
 
-	$("#map").css('background-color', '#00FF00');
+	
 
 
-    d3.json("data/kommuner_swe.json", function(error, world) {
+    d3.json("data/kommuner_swe_2.json", function(error, world) {
         
-    	//console.log(world);
+    	console.log(world);
 
 
         //var countries = topojson.feature(world, world.objects.countries).features;
