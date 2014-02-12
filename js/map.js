c15242f8b6588a8e57c122a3988e539867a2a22d
bougoue <oscar@blixtra.se>
2014-02-12 15:26:11 +0100

    Map visualization basic done.

--- js/map.js
+++ js/map.js
@@ -3,13 +3,27 @@ function Map(){
 	// Initalization
 
 	var crimeData;
+    var boundaries = {
+        "Totalt antal brott" : [100000,0],
+        "Våldsbrott" : [100000,0],
+        "Hot- kränknings- och frihetsbrott" : [100000,0],
+        "Vårdslöshet- och vållandebrott" : [100000,0],
+        "Stöldbrott" : [100000,0],
+        "Bilbrott" : [100000,0],
+        "Skadegörelsebrott" : [100000,0],
+        "Vissa trafikbrott" : [100000,0],
+        "Alkohol- och narkotikabrott" : [100000,0],
+        "Vapenbrott" : [100000,0],
+    };
+    var colors = ["#ffeda0", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#bd0026", "#800026"];
+
 	var mapDiv = $("#map");
 	var margin = { top: 20, right: 20, bottom: 20, left: 20 };
-	var width = mapDiv.width() - margin.right - margin.left + 35;
-    var height = mapDiv.height() - margin.top - margin.bottom + 35;
+	var width = mapDiv.width() - margin.right - margin.left + 39;
+    var height = mapDiv.height() - margin.top - margin.bottom + 40;
 
     var zoom = d3.behavior.zoom()
-        .scaleExtent([1, 8])
+        .scaleExtent([1, 12])
         .on("zoom", move);
 
     var svg = d3.select("#map").append("svg")
@@ -21,9 +35,9 @@ function Map(){
  		.style("position", "absolute") 
 		.style("z-index", "10")
         .style("border", 3 + "px solid #000000")
-        .style("width", 30.3 + "%")
+        .style("width", 35 + "%")
         .style("height", 35 + "px")
-        .style("left", 70 + "%")
+        .style("left", 65.5 + "%")
         .style("top", 95 + "%")
         .style("font-size", 20 + 'px')
         .style("color", '#FFFFFF')
@@ -47,6 +61,23 @@ function Map(){
         // Load geographic data
         d3.json("data/swe_mun.json", function(error, sweden) {
 
+            // Set boundary levels on each data set
+            Object.keys(crimeData).forEach(function(a) {
+                
+                Object.keys(crimeData[a]).forEach(function(b) {
+
+                    var value = parseInt(crimeData[a][b]['helår /100000']);
+
+                    if (boundaries[b][1] < value)
+                        boundaries[b][1] = value;
+
+                    if (boundaries[b][0] > value)
+                        boundaries[b][0] = value;
+
+                });
+                
+            });
+
             var geoData = topojson.feature(sweden, sweden.objects.swe_mun).features;
             draw(geoData);   
 
@@ -66,13 +97,9 @@ function Map(){
             .attr("d", path)
             .attr("id", function(d) { return d.id; })
             .attr("title", function(d) { return d.properties.name; })
-            .attr("class", function(d) { quantize(d); })
-            /*.style("fill", function(d) {
-            	var R = parseInt(Math.random()*255);
-            	var G = parseInt(Math.random()*255);
-            	var B = parseInt(Math.random()*255);
-                return 'RGB(' + R + ',' + G + ',' + B + ')'; 
-            })*/
+            .style("fill", function(d) {
+                return colors[quantize(d)]; 
+            })
             .style({ 'stroke-opacity':0.0,'stroke':'#000000' })
             .on("mousemove", function(d) {
                 
@@ -110,9 +137,20 @@ function Map(){
     }
 
     function quantize(d) {
-        //console.log(crimeData["Hela landet"]);
-    	//console.log(crimeData[d.properties.name]);
-		return "q" + Math.min(8, ~~(crimeData[d.id] * 9 / 12)) + "-5";
+        
+        var lowLevel = boundaries['Totalt antal brott'][0];
+        var highLevel = boundaries['Totalt antal brott'][1];
+
+        levelWidth = (highLevel - lowLevel)/colors.length;
+
+        var counter = 0;
+        var value = crimeData[d.properties.name]['Totalt antal brott']['helår /100000'];
+
+        while(lowLevel + (counter * levelWidth) < value && counter < colors.length-1)
+            ++counter;
+        
+        return counter;
+
 	}
 
 
